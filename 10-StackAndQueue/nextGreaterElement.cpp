a0d5d34baf645739a73b366e382f61de14f1434d
suryanshm000 <suryanshm000@gmail.com>
2021-11-30 23:23:15 +0530

    Added code of next greater element

---
+++ 10-StackAndQueue/nextGreaterElement.cpp
@@ -0,0 +1,21 @@
+// problem link: https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<long long> nextLargerElement(vector<long long> arr, int n) {
+    vector<long long> ans(n);
+    stack<long long > s;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        while (!s.empty() && arr[i] >= s.top())
+            s.pop();
+        if (!s.empty())
+            ans[i] = s.top();
+        else
+            ans[i] = -1;
+        s.push(arr[i]);
+    }
+    return ans;
+}
