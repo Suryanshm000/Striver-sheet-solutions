1f3c755529d6aee716745d72277098af84582822
suryanshm000 <suryanshm000@gmail.com>
2021-12-03 23:56:05 +0530

    Added sliding window problem

---
+++ 10-StackAndQueue/slidingWindowMaximum.cpp
@@ -0,0 +1,21 @@
+// problem link: https://leetcode.com/problems/sliding-window-maximum/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    deque<int> dq;
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) {
+        // if index is out of bound for a window
+        if (!dq.empty() && dq.front() == i - k)    dq.pop_front();
+
+        // maintaining a decreasing order
+        while (!dq.empty() && nums[dq.back()] <= nums[i])
+            dq.pop_back();
+        dq.push_back(i);
+        if (i >= k - 1)    ans.push_back(nums[dq.front()]);
+    }
+    return ans;
+}
