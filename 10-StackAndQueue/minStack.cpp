f335ef69d37a24d7cce431e4ed38f8c87f8e89de
suryanshm000 <suryanshm000@gmail.com>
2021-12-04 23:41:23 +0530

    Min stack code

---
+++ 10-StackAndQueue/minStack.cpp
@@ -0,0 +1,33 @@
+// problem link: https://leetcode.com/problems/min-stack/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class MinStack {
+public:
+    stack<pair<int, int>> st;
+    MinStack() {
+
+    }
+
+    void push(int val) {
+        if (st.empty())  st.push({val, val});
+        else {
+            int mini = min(val, st.top().second);
+            st.push({val, mini});
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
